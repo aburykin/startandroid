7a551507879cbf314605483427a406828ec8f7c4
Alexandro <tonytony9877@gmail.com>
2018-10-10 22:25:34 +0300

    completed lesson 11

---
+++ p0111resvalues/src/main/java/ru/bur/p0111resvalues/Main.java
@@ -0,0 +1,30 @@
+package ru.bur.p0111resvalues;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        LinearLayout llBottom = (LinearLayout) findViewById(R.id.llBottom);
+        TextView tvBottom = (TextView) findViewById(R.id.tvBottom);
+        Button btnBottom = (Button) findViewById(R.id.btnBottom);
+
+
+        llBottom.setBackgroundResource(R.color.llBottomColor);
+        tvBottom.setText(R.string.tvBottomText);
+        btnBottom.setText(R.string.btnBottomText);
+
+        //получить значение ресурса
+        //getResources().getString();
+
+
+    }
+}
